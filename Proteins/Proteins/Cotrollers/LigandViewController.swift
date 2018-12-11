bc37ac8a678036300325b196033fb3f4f8ac6536
cruize-labs <cruixer.labs@gmail.com>
2018-12-11 16:58:32 +0200

    Do: swiftyProtien [ complete ]

---
+++ Proteins/Proteins/Cotrollers/LigandViewController.swift
@@ -0,0 +1,156 @@
+//
+//  LigandViewController.swift
+//  Proteins
+//
+//  Created by William PHOKOMPE on 2018/11/28.
+//  Copyright © 2018 William PHOKOMPE. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class LigandViewController: UIViewController {
+    var ligand = _ligandName
+    
+    @IBOutlet weak var elementName: UILabel!
+    @IBOutlet weak var sceneView: SCNView!
+    @IBOutlet weak var ligandName: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    let data = LigandModel()
+    let cameraNode = SCNNode()
+    var thisData: String?
+    //Touch nodes
+    var oldNode: SCNNode?
+    var element: Element?
+    // Geometry
+    var geometryNode: SCNNode = SCNNode()
+    // Gestures
+    var currentAngle: Float = 0.0
+    var refreshTimer: Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.startAnimating()
+        ligandName.text = ligand
+        switch UIDevice.current.orientation {
+        case .portrait:
+            sceneSetup(orientation: "Portrait")
+        case .landscapeLeft:
+            sceneSetup(orientation: "Landscape")
+        case .landscapeRight:
+            sceneSetup(orientation: "Landscape")
+        default:
+            sceneSetup(orientation: "Landscape")
+        }
+        data.getModel(ligand: ligand!, completionHandler: { (response) in
+            DispatchQueue.main.async {
+                if let _data = response {
+                    self.ligandSelected(specs: _data)
+                    self.thisData = _data
+                } else {
+                    print("nil")
+                }
+            }
+        })
+        self.element = GetElementDetails().getDetails()
+        
+        refreshTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.backgroundCheck), userInfo: nil, repeats: true)
+    }
+    
+    @objc func backgroundCheck() {
+        let state = UIApplication.shared.applicationState
+        if state == .background {
+            self.performSegue(withIdentifier: "GoBack", sender: self.navigationController)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.oldNode?.removeAllActions()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: sceneView)
+        let hitList = sceneView.hitTest(location, options: nil)
+        
+        DispatchQueue.main.async {
+            if let hitObject = hitList.first {
+                if hitObject.node.name != "CONECT" && touches.count == 1 {
+                    
+                    self.oldNode?.removeAllActions()
+                    for elem in (self.element?.elements)! {
+                        if (elem.symbol?.lowercased() == hitObject.node.name?.lowercased()) {
+                            self.elementName.text = elem.name
+                        }
+                    }
+                }
+            } else if (touches.count > 1) {
+                self.oldNode?.removeAllActions()
+            }
+        }
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        switch UIDevice.current.orientation{
+        case .portrait:
+            sceneSetup(orientation: "Portrait")
+            self.ligandSelected(specs: self.thisData!)
+            break
+        case .portraitUpsideDown:
+            sceneSetup(orientation: "Portrait")
+            self.ligandSelected(specs: self.thisData!)
+            break
+        case .landscapeLeft:
+            sceneSetup(orientation: "Landscape")
+            self.ligandSelected(specs: self.thisData!)
+            break
+        case .landscapeRight:
+            sceneSetup(orientation: "Landscape")
+            self.ligandSelected(specs: self.thisData!)
+            break
+        default:
+            sceneSetup(orientation: "Landscape")
+            self.ligandSelected(specs: self.thisData!)
+        }
+    }
+    
+    @IBAction func shareOnClick(_ sender: Any) {
+        UIGraphicsBeginImageContextWithOptions(sceneView.bounds.size, false, UIScreen.main.scale)
+        sceneView.drawHierarchy(in: sceneView.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let activityController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    
+    func sceneSetup(orientation: String) {
+        cameraNode.removeFromParentNode()
+        sceneView.scene = nil
+        let scene = SCNScene()
+        
+        cameraNode.camera = SCNCamera()
+        if orientation == "Portrait" {
+            cameraNode.position = SCNVector3Make(0, 0, 30)
+            scene.rootNode.addChildNode(cameraNode)
+        } else {
+            cameraNode.position = SCNVector3Make(0, 0, 20)
+            scene.rootNode.addChildNode(cameraNode)
+        }
+        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+    }
+    
+    func ligandSelected(specs: String) {
+        geometryNode.removeFromParentNode()
+        currentAngle = 0.0
+        geometryNode = Molecules.drawLigand(specs: specs)
+        sceneView.scene!.rootNode.addChildNode(geometryNode)
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
+
+}
---
+++ Proteins/Proteins/Cotrollers/LoginViewController.swift
@@ -0,0 +1,88 @@
+//
+//  LoginViewController.swift
+//  Proteins
+//
+//  Created by William PHOKOMPE on 2018/11/27.
+//  Copyright © 2018 William PHOKOMPE. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var touchIdLogin: UIButton!
+    
+    @IBAction func loginOnTouch(_ sender: Any) {
+        context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Use your finger for TouchID! - or  [ alt + cmd + m ]", reply: {(Success, error) in
+            if Success {
+                print ("Success")
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "AuthDone", sender: self.navigationController)
+                }
+                self.navigationController?.dismiss(animated: false, completion: nil)
+            } else {
+                self.showAlert(title: "Error", message: "Biometeric scan did not match!", actionTitle: "OK")
+                print ("Error")
+            }
+        })
+    }
+    
+    let context: LAContext = LAContext()
+    
+    @IBAction func loginOnClick(_ sender: Any) {
+        //Theory of hitting the 42 api for authentication
+        if username.text == "" || password.text == "" {
+            self.showAlert(title: "Info: ", message: "Huh!?_ There seem to be a problem, it's definitely not on my code: Try some dummy log in", actionTitle: "Will You ? OK.")
+        } else {
+            self.performSegue(withIdentifier: "AuthDone", sender: self)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            loginButton.isEnabled = true
+            username.isEnabled = true
+            password.isEnabled = true
+            touchIdLogin.isEnabled = false
+        } else {
+            loginButton.isEnabled = false
+            username.isEnabled = false
+            password.isEnabled = false
+            touchIdLogin.isEnabled = true
+        }
+    }
+    
+    struct AppUtility {
+        
+        static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+            
+            if let delegate = UIApplication.shared.delegate as? AppDelegate {
+                delegate.orientationLock = orientation
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        AppUtility.lockOrientation(.portrait)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        AppUtility.lockOrientation(.all)
+    }
+    
+    
+    
+    func showAlert(title: String, message: String, actionTitle: String) {
+        let alert = UIAlertController(title: title, message:  message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
